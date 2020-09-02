02f2e4543f4f1834c7a6a9785078237b8e04ec3b
Damodar Dahal <underscoredam@gmail.com>
2020-09-02 06:30:21 -0700

    register models and admin models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Restaurant, Review
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'restaurant', 'rating', 'date_visited', 'comment')
