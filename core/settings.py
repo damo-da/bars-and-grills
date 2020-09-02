1387c75550494fa8269a9e80678381ae7f57441e
Damodar Dahal <underscoredam@gmail.com>
2020-09-02 05:51:14 -0700

    rename core module

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'barsandgrills.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'barsandgrills.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
