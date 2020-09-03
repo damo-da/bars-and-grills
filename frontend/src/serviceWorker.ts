880dc3156c394179aca0001b319e47cf231761a6
Damodar Dahal <underscoredam@gmail.com>
2020-09-03 11:58:09 -0700

    integrate lint

--- frontend/src/serviceWorker.ts
+++ frontend/src/serviceWorker.ts
@@ -49,6 +49,7 @@ export function register(config?: Config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
+          // tslint:disable-next-line:no-console
           console.log(
             'This web app is being served cache-first by a service ' +
               'worker. To learn more, visit https://bit.ly/CRA-PWA'
@@ -77,6 +78,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
+              // tslint:disable-next-line:no-console
               console.log(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
@@ -90,6 +92,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
+              // tslint:disable-next-line:no-console
               console.log('Content is cached for offline use.');
 
               // Execute callback
@@ -102,6 +105,7 @@ function registerValidSW(swUrl: string, config?: Config) {
       };
     })
     .catch(error => {
+      // tslint:disable-next-line:no-console
       console.error('Error during service worker registration:', error);
     });
 }
@@ -130,6 +134,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
       }
     })
     .catch(() => {
+      // tslint:disable-next-line:no-console
       console.log(
         'No internet connection found. App is running in offline mode.'
       );
@@ -143,6 +148,7 @@ export function unregister() {
         registration.unregister();
       })
       .catch(error => {
+        // tslint:disable-next-line:no-console
         console.error(error.message);
       });
   }
