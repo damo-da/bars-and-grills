d1d7c5c6161038d3fbb2183abcca1af60fa5e5a7
Damodar Dahal <underscoredam@gmail.com>
2020-09-03 12:06:52 -0700

    Use absolute imports

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
+import logo from 'assets/images/logo.svg';
 import './App.scss';
 
 function App() {
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from 'components/App/App';
+import * as serviceWorker from 'utils/serviceWorker';
 import './index.scss';
-import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
