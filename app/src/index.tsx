3f746774d372e9d29bc2002ec5de37f0204f7293
Damodar Dahal <underscoredam@gmail.com>
2020-09-07 16:21:41 -0700

    multiproject support using environment

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,3 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import * as serviceWorker from 'utils/serviceWorker';
+
 import './index.scss';
-// import 'webapp/index';
-import 'admin-panel/index';
+
+const BUILD_TARGETS = [
+  {
+    name: 'admin-panel',
+    path: './admin-panel/index',
+  },
+  {
+    name: 'webapp',
+    path: './webapp/index',
+  },
+];
+
+// Determine which entry point to import
+const { path } = BUILD_TARGETS.find(
+  ({ name }) => process.env.REACT_APP_PROJECT === name,
+) || {};
+
+// Import the entry point and render it's default export
+import(`${path}`).then(({ default: BuildTarget }) => {
+  ReactDOM.render(
+    <BuildTarget />,
+    document.getElementById('root'),
+  );
+
+  serviceWorker.unregister();
+  return null;
+});
+
+serviceWorker.unregister();
--- app/src/webapp/index.tsx
+++ app/src/webapp/index.tsx
@@ -1,22 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import * as serviceWorker from 'utils/serviceWorker';
+import App from 'webapp/components/App/App';
 
 import { initAuthModule, redirectIfNotLoggedIn } from 'webapp/utils/auth';
-import App from 'webapp/components/App/App';
 
 initAuthModule();
 redirectIfNotLoggedIn();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default App;
