b39452dfa655dba41657a2ef612e4592adc87a39
Damodar Dahal <underscoredam@gmail.com>
2020-09-19 17:52:56 -0500

    added buildspec for building and pushing API docker image

---
+++ scripts/build-docker-images.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+## Build and push docker images...
+## variables
+# AWS_ACCOUNT_ID = <id>
+# $AWS_REGION = "us-west-2"
+
+BASE_URL="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+API_REPO="barsngrills/api:latest"
+ADMIN_PANEL_REPO="barsngrills/admin-panel:latest"
+WEB_APP_REPO="barsngrills/webapp:latest"
+
+API_URL="$BASE_URL/$API_REPO"
+ADMIN_PANEL_URL="$BASE_URL/$ADMIN_PANEL_REPO"
+WEB_APP_URL="$BASE_URL/$WEB_APP_REPO"
+
+echo "Building Docker images on $(date)"
+cd backend || (echo "Failed to load backend. Execute this script from the root directory." && exit 1)
+docker build -t $API_REPO .
+echo "Tagging"
+docker tag $API_REPO "$API_URL"
+echo "Pushing"
+docker push "$API_URL"
+echo "API complete."
+cd ..
+
+echo All done.
+
