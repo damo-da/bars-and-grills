d2aa48bd260783141bef936b1e29dec4aafe4872
Damodar Dahal <underscoredam@gmail.com>
2020-09-19 20:57:09 -0500

    image definitions added

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -3,7 +3,7 @@
 ## variables
 # AWS_ACCOUNT_ID = <id>
 # AWS_REGION = "us-west-2"
-# API_URL = "https://url.com/testing"
+# API_ENDPOINT = "https://url.com/testing"
 # API_DATABASE_CONN_STR = ""
 # API_SECRET = "blahblah"
 
@@ -24,7 +24,7 @@ export SECRET_KEY="$API_SECRET"
 cd backend || (echo "Failed to load backend. Execute this script from the root directory." && exit 1)
 docker build -t $API_REPO .
 echo "Tagging"
-docker tag $API_REPO "$API_URL"
+docker tag $API_REPO "$API_ENDPOINT"
 echo "Pushing"
 docker push "$API_URL"
 echo "API complete."
@@ -52,6 +52,10 @@ echo "Pushing"
 docker push "$ADMIN_PANEL_URL"
 echo "Admin panel complete."
 cd ..
+
+printf '[{"name":"api","imageUri":"%s"}]' "api" $API_URL > imagedefinition-api.json
+printf '[{"name":"admin-panel","imageUri":"%s"}]' "api" $ADMIN_PANEL_URL > imagedefinition-admin-panel.json
+printf '[{"name":"webapp","imageUri":"%s"}]' "api" $WEB_APP_URL > imagedefinition-webapp.json
 
 echo All done.
 
